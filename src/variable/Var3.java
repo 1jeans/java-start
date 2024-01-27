aed080056ed94cbf21a58ef10ef13feae31c14b4
ham-wonjin <1jeans@github.com>
2024-01-27 19:01:24 +0900

    다시자바 1일차(변수)

--- src/variable/Var3.java
+++ src/variable/Var3.java
@@ -1,14 +1,16 @@
 package variable;
 
-public class Var2 {
-
+public class Var3 {
     public static void main(String[] args) {
-        int a;
+
+        int a ;
+
+        a = 10;
+
+        System.out.println(a);
 
         a = 20;
 
-        System.out.println(a);
-        System.out.println(a);
         System.out.println(a);
     }
 }
---
+++ src/variable/VarEx3.java
@@ -0,0 +1,14 @@
+package variable;
+
+public class VarEx3 {
+
+    public static void main(String[] args) {
+
+        long longVar = 10000000000L;
+        System.out.println(longVar);
+        boolean a = true;
+        System.out.println(a);
+
+
+    }
+}
